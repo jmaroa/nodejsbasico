d10d1ec128f12cffb36d046487d8d0b66e1ee587
janeirodigital <mario.rodriguez@janeirodigital.com>
2019-03-03 15:29:01 -0600

    Practice done

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const readLine = require("readline");
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const q = ["Cual es tu nombre? ", "Cual es tu edad? "];
+
+const AskQuestion = (rl, question) => {
+  return new Promise((resolve, reject) => {
+    rl.question(question, answer => {
+      if (!answer) {
+        reject(answer);
+      }
+
+      resolve(answer);
+    });
+  });
+};
+
+const Ask = questions => {
+  return new Promise(async resolve => {
+    let results = [];
+    for (let i = 0; i < questions.length; i++) {
+      const result = await AskQuestion(rl, questions[i]);
+      results = [...results, result];
+    }
+    rl.close();
+    resolve(results);
+  });
+};
+
+Ask(q).then(q => {
+  console.log(`Hola ${[...q]}`);
+});
