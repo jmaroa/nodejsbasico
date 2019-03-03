1ec30e062e85aaaa6a4ace20f471053defa37bb5
janeirodigital <mario.rodriguez@janeirodigital.com>
2019-03-03 17:31:26 -0600

    Practice done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require("child_process");
+
+function execCommand(command) {
+  cp.exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.log(`Error: ${err}`);
+      return;
+    }
+
+    if (stdout) {
+      console.log(`Standard out: ${stdout}`);
+    }
+
+    if (stderr) console.log("Standard err: ", stderr);
+  });
+}
+
+// execCommand("ls -al");
+execCommand("node ./src/script --base=5");
