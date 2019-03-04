1abf088581bc2d65124ae79be26319ed1888480f
janeirodigital <mario.rodriguez@janeirodigital.com>
2019-03-03 23:22:56 -0600

    Practice done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  fs.readFile("./resources/my_page.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.end(data);
+  });
+});
+
+// 65500 puertas de entrada tiene un servidor
+console.log("Iniciando server....");
+server.listen(3000);
