24c65811666a19f88bd63e391cabac7aa64b6552
janeirodigital <mario.rodriguez@janeirodigital.com>
2019-03-04 21:04:38 -0600

    Practice done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, resp) => {
+  return resp.send("Estoy en la ruta HOME");
+});
+
+app.get("/app", (req, resp) => {
+  return resp.send("Estoy en la ruta APP");
+});
+
+app.get("*", (req, resp) => {
+  return resp.send("Ups! Page not found");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running in port 3000...");
+});
